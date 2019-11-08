3abe5e4a36ee1dc84ec3ac08dc8cfb85bcc9ad3f
Hassan Rauf <hassanrauf@ncloud.hk>
2019-11-08 15:50:23 +0500

    custom button

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:custom_btn_flutter/ui/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  var Title = 'Gesture';
+  runApp(new MaterialApp(title: Title, home: new Home(title: Title)));
+}
---
+++ lib/ui/Home.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  final String title;
+
+  Home({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(title),
+      ),
+      body: new Center(
+        child: new CustomButton(),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new GestureDetector(
+      onTap: () {
+        final snackBar = new SnackBar(
+          content: new Text("Hello Gestures"),
+          backgroundColor: Theme.of(context).backgroundColor,
+          duration: new Duration(
+              hours: 0,
+              minutes: 0,
+              seconds: 0,
+              milliseconds: 4100,
+              microseconds: 0),
+        );
+        Scaffold.of(context).showSnackBar(snackBar);
+      },
+      child: new Container(
+        padding: new EdgeInsets.all(31.0),
+        decoration: new BoxDecoration(
+            color: Theme.of(context).buttonColor,
+            borderRadius: new BorderRadius.circular(5.5)),
+        child: new Text("First Button"),
+      ),
+    );
+  }
+}
